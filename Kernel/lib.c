f867848b5975e4f0e4a3684bccca12e8dd47d065
Joaquin Colonnello <joaquincolonnello@hotmail.com>
2020-09-29 02:04:06 -0300

    Fixed quantum bug in memcpy

--- Kernel/lib.c
+++ Kernel/lib.c
@@ -27,41 +27,12 @@ void * memset(void * destination, char c, size_t length)
 	return destination;
 }
 
-void * memcpy(void * destination, const void * source, size_t length)
+void * memcpy(void * restrict destination, const void * restrict source, size_t length)
 {
-	/*
-	* memcpy does not support overlapping buffers, so always do it
-	* forwards. (Don't change this without adjusting memmove.)
-	*
-	* For speedy copying, optimize the common case where both pointers
-	* and the length are word-aligned, and copy word-at-a-time instead
-	* of byte-at-a-time. Otherwise, copy by bytes.
-	*
-	* The alignment logic below should be portable. We rely on
-	* the compiler to be reasonably intelligent about optimizing
-	* the divides and modulos out. Fortunately, it is.
-	*/
-	uint64_t i;
-
-	if ((uint64_t)destination % sizeof(uint32_t) == 0 &&
-		(uint64_t)source % sizeof(uint32_t) == 0 &&
-		length % sizeof(uint32_t) == 0)
-	{
-		uint32_t *d = (uint32_t *) destination;
-		const uint32_t *s = (const uint32_t *)source;
-
-		for (i = 0; i < length / sizeof(uint32_t); i++)
-			d[i] = s[i];
-	}
-	else
-	{
-		uint8_t * d = (uint8_t*)destination;
-		const uint8_t * s = (const uint8_t*)source;
-
-		for (i = 0; i < length; i++)
-			d[i] = s[i];
-	}
-
+	char * restrict dst = destination;
+	const char * restrict src = source;
+	while(length--)
+        *dst++ = *src++;
 	return destination;
 }
 
